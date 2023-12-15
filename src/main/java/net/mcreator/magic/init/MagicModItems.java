5e3067d9f3176f7d71bf896469373db5120d4d3d
furasol2317 <153875113+furasol2317@users.noreply.github.com>
2023-12-15 09:37:04 +0900

    nantokarod

--- src/main/java/net/mcreator/magic/init/MagicModItems.java
+++ src/main/java/net/mcreator/magic/init/MagicModItems.java
@@ -19,6 +19,7 @@ import net.mcreator.magic.item.SunRodItem;
 import net.mcreator.magic.item.RainRodItem;
 import net.mcreator.magic.item.NoonRodItem;
 import net.mcreator.magic.item.NightRodItem;
+import net.mcreator.magic.item.NantokaRodItem;
 import net.mcreator.magic.item.MidnightRodItem;
 import net.mcreator.magic.item.InfinityRodItem;
 import net.mcreator.magic.item.DayRodItem;
@@ -37,6 +38,7 @@ public class MagicModItems {
 	public static final RegistryObject<Item> NIGHT_ROD = REGISTRY.register("night_rod", () -> new NightRodItem());
 	public static final RegistryObject<Item> NOON_ROD = REGISTRY.register("noon_rod", () -> new NoonRodItem());
 	public static final RegistryObject<Item> MIDNIGHT_ROD = REGISTRY.register("midnight_rod", () -> new MidnightRodItem());
+	public static final RegistryObject<Item> NANTOKA_ROD = REGISTRY.register("nantoka_rod", () -> new NantokaRodItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
