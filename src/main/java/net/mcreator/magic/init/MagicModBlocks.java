74ed5c0e709860d61e8584883f940a12a8390c74
sabosabolittle <sabosabolittle@yahoo.co.jp>
2023-12-15 10:04:11 +0900

    test02
    aho2

--- src/main/java/net/mcreator/magic/init/MagicModBlocks.java
+++ src/main/java/net/mcreator/magic/init/MagicModBlocks.java
@@ -10,10 +10,13 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
 
+import net.mcreator.magic.block.Test02Block;
 import net.mcreator.magic.block.PoisonPumpkinBlock;
 import net.mcreator.magic.MagicMod;
 
 public class MagicModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, MagicMod.MODID);
 	public static final RegistryObject<Block> POISON_PUMPKIN = REGISTRY.register("poison_pumpkin", () -> new PoisonPumpkinBlock());
+	public static final RegistryObject<Block> TEST_01FIREBLOCK = REGISTRY.register("test_01fireblock", () -> new Test01fireblockBlock());
+	public static final RegistryObject<Block> TEST_02 = REGISTRY.register("test_02", () -> new Test02Block());
 }
--- src/main/java/net/mcreator/magic/init/MagicModItems.java
+++ src/main/java/net/mcreator/magic/init/MagicModItems.java
@@ -37,6 +37,8 @@ public class MagicModItems {
 	public static final RegistryObject<Item> NIGHT_ROD = REGISTRY.register("night_rod", () -> new NightRodItem());
 	public static final RegistryObject<Item> NOON_ROD = REGISTRY.register("noon_rod", () -> new NoonRodItem());
 	public static final RegistryObject<Item> MIDNIGHT_ROD = REGISTRY.register("midnight_rod", () -> new MidnightRodItem());
+	public static final RegistryObject<Item> TEST_01FIREBLOCK = block(MagicModBlocks.TEST_01FIREBLOCK);
+	public static final RegistryObject<Item> TEST_02 = block(MagicModBlocks.TEST_02);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
