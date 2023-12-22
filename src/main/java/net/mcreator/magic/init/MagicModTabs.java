3c2890fbb2323844a8c5f502b40965e9ae63b360
sabosabolittle <sabosabolittle@yahoo.co.jp>
2023-12-22 11:08:28 +0900

    BlankStaff
    中間素材用杖

--- src/main/java/net/mcreator/magic/init/MagicModTabs.java
+++ src/main/java/net/mcreator/magic/init/MagicModTabs.java
@@ -41,7 +41,7 @@ public class MagicModTabs {
 		}
 
 		if (tabData.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-			tabData.accept(MagicModItems.QWERTYUIOP.get());
+			tabData.accept(MagicModItems.BLANKSTAFF.get());
 		}
 	}
 }
