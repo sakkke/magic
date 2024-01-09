8feb3404c0aa6bbaae25b392a4758916ebb7860b
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-09 19:43:17 +0900

    MPUIを追加

---
+++ src/main/java/net/mcreator/magic/client/screens/MPUIOverlay.java
@@ -0,0 +1,38 @@
+
+package net.mcreator.magic.client.screens;
+
+import org.checkerframework.checker.units.qual.h;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.Minecraft;
+
+@Mod.EventBusSubscriber({Dist.CLIENT})
+public class MPUIOverlay {
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public static void eventHandler(RenderGuiEvent.Pre event) {
+		int w = event.getWindow().getGuiScaledWidth();
+		int h = event.getWindow().getGuiScaledHeight();
+		Level world = null;
+		double x = 0;
+		double y = 0;
+		double z = 0;
+		Player entity = Minecraft.getInstance().player;
+		if (entity != null) {
+			world = entity.level();
+			x = entity.getX();
+			y = entity.getY();
+			z = entity.getZ();
+		}
+		if (true) {
+			event.getGuiGraphics().drawString(Minecraft.getInstance().font, Component.translatable("gui.magic.mpui.label_mp"), w / 2 + 153, h / 2 + -4, -1, false);
+		}
+	}
+}
