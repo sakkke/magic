016d5e41dad68b982f681d13a0d955df1f8d0110
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-09 18:11:04 +0900

    RainRodのMPコストを2に変更

--- src/main/java/net/mcreator/magic/item/RainRodItem.java
+++ src/main/java/net/mcreator/magic/item/RainRodItem.java
@@ -49,7 +49,7 @@ public class RainRodItem extends SwordItem {
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
 		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		RainRodRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ());
+		RainRodRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity);
 		return ar;
 	}
 
