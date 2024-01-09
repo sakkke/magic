b39ebf53aa6cf2296237319f95105099230f6311
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-09 19:54:58 +0900

    MPUtils: MPUITextプロシージャを追加

---
+++ src/main/java/net/mcreator/magic/procedures/MPUITextProcedure.java
@@ -0,0 +1,13 @@
+package net.mcreator.magic.procedures;
+
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.magic.network.MagicModVariables;
+
+public class MPUITextProcedure {
+	public static String execute(Entity entity) {
+		if (entity == null)
+			return "";
+		return "UI: " + (entity.getCapability(MagicModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MagicModVariables.PlayerVariables())).PlayerMP;
+	}
+}
